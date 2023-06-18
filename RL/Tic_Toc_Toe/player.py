0b7108d06070479fdc7589c2db5ba58d58f310b9
Aison Z <garyzhang626@gmail.com>
2023-06-18 12:13:35 -0400

    RL in Games

---
+++ RL/Tic_Toc_Toe/player.py
@@ -0,0 +1,43 @@
+import random
+import math
+from abc import ABC, abstractmethod
+
+class Player(ABC):
+    def __init__(self, letter):
+        #letter is x or o
+        self.letter = letter
+
+    @abstractmethod
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self,letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8):')
+            # we are going to check that this is a correct value by trying to cast
+            #it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the board, we also say its invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again please')
+
+        return val
